314ca1740f36fb9b2434d832c8a5eb35f9b80c30
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-23 07:17:48 -0800

    Add ajax request for search stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,13 +4,15 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.search_new_stock(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js {render partial: 'users/result'}
+        end
       else
-        flash[:error] = "We couldn't find the ticker you entered, please enter valid ticker"
+        flash.now[:error] = "We couldn't find the ticker you entered, please enter valid ticker"
         redirect_to my_portfolio_path
       end
     else
-      flash[:error] = "Please enter valid input"
+      flash.now[:error] = "Please enter valid input"
       redirect_to my_portfolio_path
     end
   end
