d57baec86473d402795d0ee67b5ac3fbb9485b82
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-23 23:46:00 -0800

    Implement ajax request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,11 +9,15 @@ class StocksController < ApplicationController
         end
       else
         flash.now[:error] = "We couldn't find the ticker you entered, please enter valid ticker"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          format.js {render partial: 'users/result'}
+        end
       end
     else
       flash.now[:error] = "Please enter valid input"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        format.js {render partial: 'users/result'}
+      end
     end
   end
 
