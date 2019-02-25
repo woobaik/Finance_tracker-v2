4008f5670179f0b8464cd4dc1e87ede1c007dfc4
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-24 17:29:55 -0800

    Implement instance methods for can_add_stocks?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  def already_added?(ticker)
+    stock = Stock.find_by_ticker(ticker.upcase)
+    return true if stock && user_stocks.where(stock_id: stock.id)
+    false
+  end
+
+  def under_stock_limit?
+    user_stocks.count < 10
+  end
+
+  def can_add_stock?(ticker)
+    already_added?(ticker) && under_stock_limit?
+  end
+
+
 end
