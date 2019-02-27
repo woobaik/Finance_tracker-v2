8bf9bc8d8d9210e6c348f33b54aaf507c9ae1439
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-26 21:20:20 -0800

    Create first_name, last_name attributes for user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: { registrations: 'user/registrations'}
   root to:'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'my_portfolio', to: 'users#my_portfolio'
