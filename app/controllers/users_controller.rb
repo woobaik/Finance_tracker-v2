d6849e50b17675939dce7be44d63603c4ebf0a1d
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-02 18:20:50 -0800

    Finish the project with adding show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find_by_id(params[:id])
   end
 
   def add_friend
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,4 +42,5 @@ class User < ApplicationRecord
   end
 
 
+
 end
